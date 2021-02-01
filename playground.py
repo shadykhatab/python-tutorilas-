133e1a13008cd9d2242ec60436900d0a9b35b6f2
iachieve <mhalim80@hotmail.com>
2021-02-01 10:28:21 -0800

    add 110 & 120 videos

--- playground.py
+++ playground.py
@@ -1,4 +1,16 @@
-sum = 0
-for i in range(1, 11): 
-    sum = sum + i 
-print("Sum of first 10 natural number :", sum) 
+"""
+Control flow if, elif, else
+a: "90 - 100"
+b: 80 - 90
+c : 70 - 80
+"""
+grade = 50
+
+if 90 <= grade <= 100:
+    print("A")
+elif 80 <= grade <= 89:
+    print("B")
+elif 70 <= grade <= 79:
+    print("C")
+else:
+    print("F")
